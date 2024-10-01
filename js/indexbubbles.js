fd32f0a1b898a5daf925313dcdcb3f7dec746140
Sydney <sydney@meancat.com>
2024-10-01 11:36:40 -0700

    bubbles, besides bg color

---
+++ js/indexbubbles.js
@@ -0,0 +1,33 @@
+
+
+$('body').bubble({
+    background: {
+      0:'#e93479',
+      1:'#f9ac53'
+    },
+})
+
+$('body').bubble({
+    img:[
+            './images/bluestarbubble.png',
+            './images/greenstarbubble.png',
+            './images/orangestarbubble.png'
+          ],
+        
+    imgSize: { 
+         min:30,
+         max:80  
+             },
+    imgs:false,
+    bubbles:false,
+    
+        
+    })
+
+   
+        
+    
+        
+      
+            
+    

b781797c31e63676bd86cce12704646898b1240f
Sydney <sydney@meancat.com>
2024-10-17 22:10:21 -0700

    please be done

--- js/transformationsjs.js
+++ js/transformationsjs.js
@@ -1,14 +1,45 @@
-
-
-function scrollPersona(){
-    document.getElementById('personaContainer').scrollIntoView();
-  }
-
-  function scrollGenshin(){
+window.onload = function() {
     document.getElementById('genshinContainer').scrollIntoView();
-  }
+  };
 
-  function scrollBrokenReality(){
+function scrollPersona() {
+    document.getElementById('personaContainer').scrollIntoView();
+    document.getElementById('personaContainer').style.visibility = 'visible'; 
+    document.getElementById('personaContainer').style.opacity = '1';
+
+    document.getElementById('genshinContainer').style.opacity = '1'; 
+    setTimeout(() => {
+        document.getElementById('genshinContainer').style.opacity = '0'; 
+    }, 0)
+
+    document.getElementById('brokenrealityContainer').style.opacity = '1'; 
+    setTimeout(() => {
+        document.getElementById('brokenrealityContainer').style.opacity = '0'; 
+    }, 0)
+}
+
+function scrollGenshin() {
+    document.getElementById('genshinContainer').scrollIntoView();
+    document.getElementById('genshinContainer').style.visibility = 'visible'; 
+    document.getElementById('genshinContainer').style.opacity = '1';
+
+    document.getElementById('personaContainer').style.opacity = '1'; 
+    setTimeout(() => {
+        document.getElementById('personaContainer').style.opacity = '0'; 
+    }, 0)
+}
+
+function scrollBrokenReality() {
     document.getElementById('brokenrealityContainer').scrollIntoView();
-  }
- 
+    document.getElementById('brokenrealityContainer').style.visibility = 'visible'; 
+    document.getElementById('brokenrealityContainer').style.opacity = '1';
+
+    document.getElementById('personaContainer').style.opacity = '1'; 
+    setTimeout(() => {
+        document.getElementById('personaContainer').style.opacity = '0'; 
+    }, 0)
+}
+
+
+
+

52886f817408e7f9ab7ae23731ef02f714a39ba0
Sydney <sydney@meancat.com>
2024-10-17 18:52:32 -0700

    buttons good
    center the title and maybe make it look better other than that its alright i guess.

--- js/transformationsjs.js
+++ js/transformationsjs.js
@@ -1,7 +1,14 @@
 
 
-function scrollRight(){
+function scrollPersona(){
     document.getElementById('personaContainer').scrollIntoView();
   }
 
+  function scrollGenshin(){
+    document.getElementById('genshinContainer').scrollIntoView();
+  }
+
+  function scrollBrokenReality(){
+    document.getElementById('brokenrealityContainer').scrollIntoView();
+  }
  
